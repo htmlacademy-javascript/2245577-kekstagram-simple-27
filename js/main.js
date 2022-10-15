bb0792eaa0b00791f260eb6279cb50058ed0f13a
ValeraVolkogonov <114488509+ValeraVolkogonov@users.noreply.github.com>
2022-10-15 21:28:45 +0300

    Создал две функции

---
+++ js/main.js
@@ -0,0 +1,17 @@
+function getRandomPositiveInteger (min, max) {
+
+  if (min < 0 || max < 0) {
+    return NaN;
+  }
+
+  const lower = Math.ceil(Math.min(min, max));
+  const upper = Math.floor(Math.max(min, max));
+
+  const result = Math.random() * (upper - lower + 1) + lower;
+
+  return Math.floor(result);
+};
+
+function checkStringLength (string, length) {
+  return string.length <= length;
+};
