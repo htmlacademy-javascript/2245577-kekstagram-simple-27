fb31fb268d92133382876d53236ac01fd41c7002
ValeraVolkogonov <114488509+ValeraVolkogonov@users.noreply.github.com>
2022-10-21 22:34:51 +0300

    Написал код для симуляции фото

--- js/main.js
+++ js/main.js
@@ -1,17 +1,41 @@
-function getRandomPositiveInteger (min, max) {
+function getRandomPositiveInteger(a, b) {
 
-  if (min < 0 || max < 0) {
+  if (a < 0 || b < 0) {
     return NaN;
   }
-
-  const lower = Math.ceil(Math.min(min, max));
-  const upper = Math.floor(Math.max(min, max));
-
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
   const result = Math.random() * (upper - lower + 1) + lower;
-
   return Math.floor(result);
-};
+}
 
-function checkStringLength (string, length) {
+function checkStringLength(string, length) {
   return string.length <= length;
-};
+}
+
+let picturesCount = 25;
+
+let photoDescription = [
+  'На пляже',
+  'Сегодня отличная погода',
+  'Зацените какая  картина у меня получилась',
+  'Пересел на привильное питание',
+  'Вечериний турнир по Fifa',
+  'С любимым человеком',
+  'Новый ремонт в квартире!',
+  'Утренняя пробежка по парку',
+  'Обучение программированию)',
+];
+
+let getRandomArrayElement = (elements) => elements[getRandomPositiveInteger(0, elements.length - 1)];
+
+let createPhoto = (index) => ({
+  id: index,
+  url: 'photos/${index + 1}.jpg',
+  description: getRandomArrayElement(photoDescription),
+  likes: getRandomPositiveInteger(15, 200),
+  comments: getRandomPositiveInteger(0, 200),
+});
+
+let photoSimulation = Array.from({ length: createPhoto }, (x, index) => createPhoto(index));
+console.log(photoSimulation);
