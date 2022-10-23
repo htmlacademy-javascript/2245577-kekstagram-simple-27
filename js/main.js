fa6c1b3b7ccdfe110212a69787e82aac29d46288
ValeraVolkogonov <114488509+ValeraVolkogonov@users.noreply.github.com>
2022-10-23 17:06:27 +0300

    Исправление ошибок: const вместо let

--- js/main.js
+++ js/main.js
@@ -36,6 +36,5 @@ const createPhoto = (index) => ({
   likes: getRandomPositiveInteger(15, 200),
   comments: getRandomPositiveInteger(0, 200),
 });
-
-let photoSimulation = Array.from({ length: createPhoto }, (x, index) => createPhoto(index));
+ const photoSimulation = Array.from({ length: createPhoto }, (x, index) => createPhoto(index));
 console.log(photoSimulation);
